b92c9f812614c281831dcd9121bdfa7367fa72be
ebregoli <bregolienrio@gmail.com>
2026-04-27 17:41:32 +0200

    Backend: aggiunto vincolo di unicità mancante sul nome della categoria

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_27_142958) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_27_153805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_27_142958) do
     t.text "description"
     t.string "name"
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "products", force: :cascade do |t|
