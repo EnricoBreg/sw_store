68faf4094644785562c8b72e40c37071b77b0b97
ebregoli <bregolienrico@gmail.com>
2026-05-01 14:59:54 +0200

    Backend: lanciate migrazioni dal commit precedente

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_01_103922) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_01_125402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -54,6 +54,32 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_01_103922) do
     t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.decimal "discount_percentage"
+    t.bigint "order_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity"
+    t.decimal "unit_price"
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "card_number"
+    t.string "city"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.string "status"
+    t.string "street"
+    t.decimal "total_amount"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.string "zip_code"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.boolean "active"
     t.bigint "category_id", null: false
@@ -91,5 +117,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_01_103922) do
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "products"
   add_foreign_key "carts", "users"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "categories"
 end
