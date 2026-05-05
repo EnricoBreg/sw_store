07a4f424f8a7bf78eccb8f77d95396ed04154b64
ebregoli <bregolienrio@gmail.com>
2026-05-05 12:46:46 +0200

    Frontend: aggiunti commenti per descrizione delle varie property.

--- frontend/src/app/core/models/api-types.ts
+++ frontend/src/app/core/models/api-types.ts
@@ -1,17 +1,17 @@
 export interface ApiResponse<T> {
-  success: boolean;
-  message?: string;
-  data: T;
-  meta?: PaginationMeta;
-  errors?: string[];
+  success: boolean; // Flag che indica il successo o meno della richiesta
+  message?: string; // Eventuale messaggio inviato da Backend in risposta ad una richiesta (non sempre presente)
+  data: T;          // Tipo di dato della rispota (es. Product[], Product, ...)
+  meta?: PaginationMeta;  // Tipo di dato per i meta della paginazione (se presente)
+  errors?: string[];  // Array di errori (stringhe) descrittive dell'eventuale errore verificatosi
 }
 
 export interface PaginationMeta {
-  count: number;
-  limit: number;
-  page: number;
-  next: number;
-  prev: number;
-  next_url: string;
-  prev_url: string;
+  count: number;  // Numero totale dei record provenienti dal Backend
+  limit: number;  // Numero di record per richiesta (impostabile tramite ?limit=...)
+  page: number;   // Numero corrente di pagina
+  next: number;   // Numero di pagina successivo
+  prev: number;   // Numero di pagina precedente
+  next_url: string; // URL per raggiungere la prossima pagina
+  prev_url: string; // URL per raggiungere la precedente pagina
 }
