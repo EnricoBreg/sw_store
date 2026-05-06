d6e8360c1bad73b049e73c50a3b63ea7d3c894f1
ebregoli <bregolienrio@gmail.com>
2026-05-06 18:05:53 +0200

    Backend: aggiornato url di callback lato frontend.

--- backend/app/controllers/auth/omniauth_callbacks_controller.rb
+++ backend/app/controllers/auth/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       token = @user.generate_twt
 
-      redirect_to "http://localhost:4200/#/oauth/callback?token=#{token}", allow_other_host: true
+      redirect_to "http://localhost:4200/oauth/callback?token=#{token}", allow_other_host: true
     else
       Rails.logger.error "User persist failed: #{@user.errors.full_messages}"
       redirect_to "http://localhost:4200/login?error=oauth_failed", allow_other_host: true
