8e8eb7e13cca06d8fc7095efc04d07477fbb4946
ebregoli <bregolienrico@gmail.com>
2026-05-28 22:40:20 +0200

    Backend: aggiunti nuovi attributi nel serializer per l'utente, utili nel frontend.

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -9,4 +9,17 @@ class UserSerializer
   attribute :date_of_birth do |user|
     user.date_of_birth && user.date_of_birth.strftime("%d/%m/%Y")
   end
+
+  attribute :orders_count do |user|
+    user.orders.size
+  end
+
+  attribute :last_order_date do |user|
+    last_order = user.orders.last
+    last_order ? last_order.created_at.strftime("%d/%m/%Y") : nil
+  end
+
+  attribute :addresses_count do |user|
+    user.addresses.size
+  end
 end
