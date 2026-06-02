897afa4f021f1b8d8176457f88833615c17f09d5
ebregoli <bregolienrico@gmail.com>
2026-06-02 17:32:36 +0200

    Frontend: aggiunto allineamento al centro del testo.

--- frontend/src/app/core/directives/badge.ts
+++ frontend/src/app/core/directives/badge.ts
@@ -3,7 +3,7 @@ import { Directive } from "@angular/core";
 @Directive({
   selector: "[appBadge]",
   host: {
-    class: "inline-flex items-center px-3 py-1 rounded-full text-sm ring-1 ring-inset font-medium",
+    class: "inline-flex items-center px-3 py-1 rounded-full text-sm text-center ring-1 ring-inset font-medium",
   },
 })
 export class Badge {
