6dda2ec50f64a774c3915a2dc7dc71f399ad53f6
ebregoli <bregolienrico@gmail.com>
2026-06-02 17:38:10 +0200

    Frontend: ridotto il margin-bottom per i pannelli interni dell'applicazione.

--- frontend/src/app/core/directives/inner-panel.ts
+++ frontend/src/app/core/directives/inner-panel.ts
@@ -3,7 +3,7 @@ import { Directive } from "@angular/core";
 @Directive({
   selector: "[appInnerPanel]",
   host: {
-    class: "rounded-2xl border border-gray-200/70 shadow-xs p-6 mt-4 mb-8 overflow-x-auto"
+    class: "rounded-2xl border border-gray-200/70 shadow-xs p-6 mt-4 mb-5 overflow-x-auto"
   }
 })
 export class InnerPanel {
